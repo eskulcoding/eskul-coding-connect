d480219f94ae8f5d4564602466788949249771a9
eskulcoding <157893890+eskulcoding@users.noreply.github.com>
2024-01-29 20:15:36 +0700

    Add files via upload

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,15 +5,15 @@ import Image from "next/image";
 export default function Profile() {
     return (
         <div className='w-full flex flex-col items-center space-y-5 text-center mb-10'>
-          <div className='w-24 h-24 rounded-full cursor-pointer relative overflow-hidden bg-[#16171B] dark:bg-white duration-200'>
-            <Image src={"/img/logo-0.jpg"} width={200} height={200} alt="eskul coding sma perguruan buddhi" layout="responsive"/>
+          <div className='w-20 h-20 2xl:w-24 2xl:h-24 rounded-full cursor-pointer relative overflow-hidden bg-[#16171B] dark:bg-white duration-200'>
+            <Image src={"/assets/img/logo-0.jpg"} width={200} height={200} alt="eskul coding sma perguruan buddhi" layout="responsive"/>
           </div>
           <div className='grid dark:text-neutral-200 duration-200'>
             <div className='flex space-x-2 items-center justify-center'>
-              <span className='text-2xl font-bold'>Eskul Coding SMA Buddhi</span>
+              <span className='text-xl 2xl:text-2xl font-bold'>Eskul Coding SMA Buddhi</span>
               <FontAwesomeIcon icon={faMicrochip} className='text-orange-400 text-2xl cursor-pointer'/>
             </div>
-            <span>Coding extracurricular at Buddhi College High School</span>
+            <span className="text-sm xl:text-base">Coding extracurricular at Buddhi College High School</span>
           </div>
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,20 +4,25 @@ import Switch from "@/components/Switch";
 import Profile from "@/components/Profile";
 import Links from "@/components/Links";
 import LetsConnect from "@/components/LetsConnect";
+import LoadingScreen from "@/components/ui/LoadingScreen";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-    className={`w-full min-h-screen grid place-content-center bg-white dark:bg-[#101010] duration-100 ${inter.className}`}
-  >
-    <Switch/>
-    <div className='py-20 md:py-0 w-[50vh] md:w-[43vh] lg:w-[40vh] p-5 flex flex-col justify-center item-center'>
-      <Profile/>
-      <Links/>
-      <LetsConnect/>
-    </div>
-  </main>
+    <>
+      <header>
+        <title>Eskul coding | connect</title>
+      </header>
+      <LoadingScreen/>
+      <main className={`w-full min-h-screen grid place-content-center bg-white dark:bg-[#101010] duration-100 ${inter.className} overflow-hidden`}>
+        <Switch/>
+        <div className='py-20 md:py-0 w-full md:w-[43vh] lg:w-[50vh] p-5 flex flex-col justify-center item-center'>
+          <Profile/>
+          <Links/>
+          <LetsConnect/>
+        </div>
+      </main>
+    </>
   );
 }
