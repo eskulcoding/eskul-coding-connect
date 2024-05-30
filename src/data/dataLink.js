49df74cbe6e7d5a5bdbf0cfb5ad6abda59b60bb2
eskulcoding <157893890+eskulcoding@users.noreply.github.com>
2024-05-30 11:04:08 +0700

    Update dataLink.js

--- src/data/dataLink.js
+++ src/data/dataLink.js
@@ -1,4 +1,4 @@
-export const websiteUrl = "https://eskulcoding.netlify.app/";
+export const websiteUrl = "https://smadhicoding.netlify.app/";
 export const instagramUrl = "https://www.instagram.com/smadhi_coding";
 export const githubUrl = "https://github.com/eskulcoding";
 export const emailUrl = "mailto:eskulcoding.smabuddhi@gmail.com";
